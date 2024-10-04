921012d6d0d1cb0f5a8fd8f986c5dcc9ed737174
Abhijith Babu <abijithb2002@gmail.com>
2024-10-05 00:14:33 +0530

    fix:window not defined error

--- client/app/components/google/index.js
+++ client/app/components/google/index.js
@@ -1,40 +1,40 @@
 'use client';
-
 import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
 import Image from 'next/image';
-import { gapi } from 'gapi-script';
 import axios from 'axios';
-// import { FaGoogle } from 'react-icons/fa';
 
 const GoogleLoginComponent = () => {
   const [isLoading, setIsLoading] = useState(false);
   const router = useRouter();
 
   useEffect(() => {
-    function start() {
-      gapi.client.init({
-        clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
-        scope: 'profile email',
-      }).then(() => {
-        gapi.auth2.getAuthInstance();
-      });
+    if (typeof window !== 'undefined') {
+      const loadGoogleApi = () => {
+        const script = document.createElement('script');
+        script.src = 'https://apis.google.com/js/platform.js';
+        script.async = true;
+        script.onload = () => {
+          window.gapi.load('auth2', () => {
+            window.gapi.auth2.init({
+              client_id: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
+              scope: 'profile email',
+            });
+          });
+        };
+        document.body.appendChild(script);
+      };
+      loadGoogleApi();
     }
-    gapi.load('client:auth2', start);
   }, []);
 
   const handleLogin = () => {
     setIsLoading(true);
-    console.log('handleLogin')   
-    const auth2 = gapi.auth2.getAuthInstance();
-    console.log('auth2',auth2)   
+    const auth2 = window.gapi.auth2.getAuthInstance();
     auth2.signIn().then(async (googleUser) => {
       const id_token = googleUser.getAuthResponse().id_token;
-      console.log('id_token',id_token);
-      
       try {
         const res = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/auth/google-login`, { tokenId: id_token });
-        console.log('res-',res);
         localStorage.setItem('Tokens', JSON.stringify({ access: res.data.token.accessToken, refresh: res.data.token.refreshToken }));
         setIsLoading(false);
         router.push('/');
@@ -61,8 +61,7 @@ const GoogleLoginComponent = () => {
         className="w-full flex items-center justify-center bg-white border border-gray-300 p-2 rounded hover:bg-gray-50 mb-4"
       >
         <Image src="/assets/google-icon.png" alt="Google" width={20} height={20} className="mr-2" />
-        {/* <FaGoogle className="mr-2" /> */}   
-         Continue with Google
+        Continue with Google
       </button>
     </>
   );
